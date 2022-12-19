8d3d43e72572f466e00f3724f989a1819b4690dc
Duhan Ozturk <duhan9885@gmail.com>
2022-12-19 17:13:55 +0300

    Okul Yonetimi Liste Guncelleme

---
+++ enums/Role.java
@@ -0,0 +1,21 @@
+package AdvJavaPractice.enums;
+
+public enum Role {
+
+    ROLE_ADMIN("admin"),
+    ROLE_TEACHER("teacher"),
+    ROLE_STUDENT("student");
+
+    private final String name;
+
+    Role(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+
+
+}
